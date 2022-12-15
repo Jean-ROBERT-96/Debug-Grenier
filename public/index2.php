06ba9ff22e0974d9bd1bae8e0913e77480cd6fda
Henry-Tourraine <henry.tourraine@outlook.fr>
2022-12-15 13:55:26 +0100

    mount volume false path

--- public/index2.php
+++ public/index2.php
@@ -3,7 +3,7 @@
 
     $dsn = 'mysql:host=mariadb;dbname=videgrenierenligne;charset=utf8';
     $db = new PDO($dsn, 'webapplication', '653rag9T');
-    
+    echo "db set";
     // Throw an Exception when an error occurs
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
