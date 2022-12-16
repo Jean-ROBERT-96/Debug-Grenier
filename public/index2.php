ce2cd25ee0c8bb0a52d5f83b8a2edd99f61247f2
Henry-Tourraine <henry.tourraine@outlook.fr>
2022-12-16 16:55:36 +0100

    test bdd connection#1

--- public/index2.php
+++ public/index2.php
@@ -1,10 +1,4 @@
 <?php
-
-
-    
-
-
-
     try {
         $dsn = 'mysql:host=mariadb;dbname=videgrenierenligne;charset=utf8';
         $db = new PDO($dsn, 'webapplication', '653rag9T');
@@ -12,7 +6,9 @@
         // Throw an Exception when an error occurs
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           echo "Connected successfully";
-        } catch(PDOException $e){
-        echo "Connection failed: " . $e->getMessage();
+     } catch(PDOException $e){
+
+          echo "Connection failed: " . $e->getMessage();
+
         }
 ?>
